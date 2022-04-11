58d06c89d60e4755541e13420bf5d14ca76febc2
innovative-mind <akporuruconfidence1@gmail.com>
2022-04-11 13:53:25 -0700

    task 6

--- 0x00-hello_world/4-puts.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-
-int main(void)
-{
-  char Mystr[] = "Programming is like building a multilingual puzzle\n";
- int puts(Mystr);
-  return (0);
-}
--- 0x00-hello_world/5-printf.c
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gcc 5-main.c
