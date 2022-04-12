9f32825ca8e92160c4f9a4cae82be020cb33abf2
innovative-mind <akporuruconfidence1@gmail.com>
2022-04-12 14:46:12 -0700

    task 6.7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+return (n * (-1));
+}
+else if (n == 0)
+{
+return (0);
+}
+else
+{
+return (n);
+}
+}
