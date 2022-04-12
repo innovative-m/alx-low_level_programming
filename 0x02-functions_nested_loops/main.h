81d80dd272beca83f8d488d03a411a091270a377
innovative-mind <akporuruconfidence1@gmail.com>
2022-04-12 13:26:25 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,10 @@
-#ifnndef MAIN_H
-#DEFINE MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-Int _Isalpha(Int c);
+int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
