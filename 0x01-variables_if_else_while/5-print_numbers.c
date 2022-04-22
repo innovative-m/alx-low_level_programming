d7b2c85457609d713865bab1d0a5fba6089c07e2
innovative-mind <akporuruconfidence1@gmail.com>
2022-04-22 09:31:36 -0700

    g

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int c;
+for (c = 0; c <= 9; c++)
+{
+printf("%d", c);
+}
+printf("\n");
+return (0);
+}
