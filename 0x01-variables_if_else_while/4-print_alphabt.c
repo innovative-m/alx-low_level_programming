78513474d76e88233701c1db30879097053bf7eb
innovative-mind <akporuruconfidence1@gmail.com>
2022-04-22 09:15:31 -0700

    f

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c != 'q' && c != 'e')
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
+  
