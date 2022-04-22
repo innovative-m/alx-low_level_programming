cc01a1a7ade9c732e999ae1a26a132e5c422a3c6
innovative-mind <akporuruconfidence1@gmail.com>
2022-04-22 09:00:47 -0700

    k

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * 
+ * Return: always 0
+ */
+void main(void)
+{
+char c;
+char C;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+for (C = 'A'; C <= 'Z'; C++)
+{
+putchar(C);
+}
+putchar('\n');
+return (0);
+}
