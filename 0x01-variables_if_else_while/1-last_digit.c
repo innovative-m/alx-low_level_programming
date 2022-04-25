a3331eb91b28ebd6a6593012bf4bf9864cfe7163
innovative-mind <akporuruconfidence1@gmail.com>
2022-04-25 16:47:46 -0700

    ta

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 /**
  * main - Entry point
  *
@@ -15,7 +18,7 @@ int lastnum;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-if(n < 0)
+if (n < 0)
 {
 mod = -10;
 lastnum = n % mod;
