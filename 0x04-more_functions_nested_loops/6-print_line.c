c9ec5403dec0313ddc6cb6725c5f176caa0bac84
innovative-mind <akporuruconfidence1@gmail.com>
2022-04-26 09:24:03 -0700

    hh

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n: first parameter.
+ *
+ */
+void print_line(int n)
+{
+int i = 1;
+
+if (n  <= 0)
+{
+_putchar('\n');
+}
+else
+{
+while (i <= n)
+{
+_putchar('_');
+i++;
+}
+_putchar('\n');
+}
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal.
+ * @n: first parameter.
+ *
+ */
+void print_diagonal(int n)
+{
+int rowNum, spaces;
+
+if (n <= 0)
+{
+_putchar('\n');
+}
+for (rowNum = 1; rowNum <= n; rowNum++)
+{
+for (spaces = 1; spaces <= (rowNum - 1); spaces++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
