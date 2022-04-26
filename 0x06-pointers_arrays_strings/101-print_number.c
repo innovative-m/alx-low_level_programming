072a9bbcd6c986e1b8817a22e6a2fa686e68222e
innovative-mind <akporuruconfidence1@gmail.com>
2022-04-25 17:12:17 -0700

    ss

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,13 +1,17 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * print_number - print an integer
- * @n
+ * @n: para
  *
- *Return: int
+ * Return: int
  */
 void print_number(int n)
 {
+  
 int j;
+ 
 j = _putchar(n);
+ 
 return (j);
 }
