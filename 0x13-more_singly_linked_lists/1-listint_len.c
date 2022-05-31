563632bd251a51c7a7611e056fcc4146af04e3f6
innovative-mind <akporuruotar@gmail.com>
2022-05-31 12:07:14 -0700

    task modification

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,77 +1,20 @@
 #include <stdio.h>
-
-
-
 #include "lists.h"
 
-
-
-
-
-
-
 /**
-
-
-
- * listint_len - computes the number of nodes in the listint_t list.
-
-
-
- * @h: pointer to the struct node list.
-
-
-
- *
-
-
-
- * Return: the number of elements.
-
-
-
- */
-
-
-
+  * listint_len - computes the number of nodes in the listint_t list.
+  * @h: pointer to the struct node list.
+  *
+  * Return: the number of elements.
+  */
 size_t listint_len(const listint_t *h)
-  
-
-  
 {
-  
+	unsigned int counter = 0;
 
-  
-  unsigned int counter = 0;
-  
-
-  
-
-  
-
-  
-  while (h != NULL)
-    
-
-    
-    {
-      
-
-      
-      h = h->next;
-      
-
-      
-      counter++;
-      
-
-      
-    }
-  
-
-  
-  return (counter);
-  
-
-  
+	while (h != NULL)
+	{
+		h = h->next;
+		counter++;
+	}
+	return (counter);
 }
