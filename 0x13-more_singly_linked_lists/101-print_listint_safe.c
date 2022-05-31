c580dc364261cc1beebc6b1116d84508e0cf6a2b
innovative-mind <akporuruotar@gmail.com>
2022-05-31 12:27:41 -0700

    3rd modification of tasks

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,149 +1,38 @@
 #include "lists.h"
-
-
-
 #include <stdio.h>
-
-
-
 #include <stdlib.h>
 
-
-
-
-
-
-
 /**
-
-
-
- * print_listint_safe - print lists with a loop.
-
-
-
- * @head: poniter the head node
-
-
-
- *
-
-
-
- * Return: number of nodes.
-
-
-
- */
-
-
-
+  * print_listint_safe - print lists with a loop.
+  * @head: poniter the head node
+  *
+  * Return: number of nodes.
+  */
 size_t print_listint_safe(const listint_t *head)
-  
-
-  
 {
-  
+	unsigned int lsize = 0, i;
+	const listint_t *ptr[200];
+	void *p = (void *)head;
 
-  
-  unsigned int lsize = 0, i;
-  
+	if (!head)
+		exit(98);
 
-  
-  const listint_t *ptr[200];
-  
-
-  
-  void *p = (void *)head;
-  
-
-  
-
-  
-
-  
-  if (!head)
-    
-
-    
-    exit(98);
-  
-
-  
-
-  
-
-  
-  while (head)
-    
-
-    
-    {
-      
-
-      
-      for (; i < lsize; i++)
-	
-
-	
+	while (head)
 	{
-	  
+		for (; i < lsize; i++)
+		{
+			if (ptr[i] == head)
+			{
+				printf("-> [%p] %d\n", p, head->n);
+				return (lsize);
+			}
+		}
 
-	  
-	  if (ptr[i] == head)
-	    
-
-	    
-	    {
-	      
-
-	      
-	      printf("-> [%p] %d\n", p, head->n);
-	      
-
-	      
-	      return (lsize);
-	      
-
-	      
-	    }
-	  
-
-	  
+		printf("[%p] %d\n", p, head->n);
+		ptr[lsize] = head;
+		lsize++;
+		head = head->next;
 	}
-      
 
-      
-
-      
-
-      
-      printf("[%p] %d\n", p, head->n);
-      
-
-      
-      ptr[lsize] = head;
-      
-
-      
-      lsize++;
-      
-
-      
-      head = head->next;
-      
-
-      
-    }
-  
-
-  
-
-  
-
-  
-  return (lsize);
-  
-
-  
+	return (lsize);
 }
