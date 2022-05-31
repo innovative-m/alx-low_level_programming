3b9e1b12aba8ec9aa6bed6e7d948e9c3ad8daa87
innovative-mind <akporuruotar@gmail.com>
2022-05-31 12:24:11 -0700

    second modification on the stingly limked list tasks

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,93 +1,24 @@
 #include "lists.h"
-
-
-
 #include <stdlib.h>
 
-
-
-
-
-
-
 /**
-
-
-
- * free_listint2 - frees listint_t list.
-
-
-
- * @head: pointer to listint_t list.
-
-
-
- *
-
-
-
- */
-
-
-
+  * free_listint2 - frees listint_t list.
+  * @head: pointer to listint_t list.
+  *
+  */
 void free_listint2(listint_t **head)
-  
-
-  
 {
-  
+	listint_t *tmp;
 
-  
-  listint_t *tmp;
-  
+	if (!head || !*head)
+		return;
 
-  
+	while (*head)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
 
-  
-
-  
-  if (!head || !*head)
-    
-
-    
-    return;
-  
-
-  
-
-  
-
-  
-  while (*head)
-    
-
-    
-    {
-      
-
-      
-      tmp = *head;
-      
-
-      
-      *head = (*head)->next;
-      
-
-      
-      free(tmp);
-      
-
-      
-    }
-  
-
-  
-
-  
-
-  
-  *head = NULL;
-  
-
-  
+	*head = NULL;
 }
--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,105 +1,27 @@
 #include "lists.h"
-
-
-
 #include <stdlib.h>
 
-
-
-
-
-
-
 /**
-
-
-
- * pop_listint - Deletes the first node.
-
-
-
- * @head: pointer to the head of the listint_t list.
-
-
-
- *
-
-
-
- * Return: Value of the node.
-
-
-
- */
-
-
-
+  * pop_listint - Deletes the first node.
+  * @head: pointer to the head of the listint_t list.
+  *
+  * Return: Value of the node.
+  */
 int pop_listint(listint_t **head)
-  
-
-  
 {
-  
+	listint_t *tmp;
 
-  
-  listint_t *tmp;
-  
+	unsigned int i;
 
-  
+	tmp = *head;
 
-  
+	if (tmp == NULL)
+		return (0);
 
-  
-  unsigned int i;
-  
+	i = tmp->n;
+	*head = tmp->next;
 
-  
+	free(tmp);
 
-  
-
-  
-  tmp = *head;
-  
-
-  
-
-  
-
-  
-  if (tmp == NULL)
-    
-
-    
-    return (0);
-  
-
-  
-
-  
-
-  
-  i = tmp->n;
-  
-
-  
-  *head = tmp->next;
-  
-
-  
-
-  
-
-  
-  free(tmp);
-  
-
-  
-
-  
-
-  
-  return (i);
-  
-
-  
+	return (i);
 }
